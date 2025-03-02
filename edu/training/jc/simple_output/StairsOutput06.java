c5e736a76d51748b508eaa97f98ec103f0a19cd3
IlyaSachenko <IlyaSachenko@yandex.by>
2025-03-03 00:03:45 +0300

    Add files via upload

---
+++ edu/training/jc/simple_output/StairsOutput06.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput06 {
+
+	public static void main(String[] args) {
+		System.out.println("\"To wisely live your life, you dont need to know much\n\n\tJust re,e,ber two ,ain rules for the begining: \n\n\t\tYou better starve, than eat whatever \n\n\tAnd better be alone, than with whoewer.\" \n\nOmar Khayyam, Rubaiyat");
+
+	}
+
+}
